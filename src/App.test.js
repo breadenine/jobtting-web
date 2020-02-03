fed9ee3ed5ebf546a6377163ba36afef7afe8747
khwan <rudghks7816@gmail.com>
2020-02-03 17:00:09 +0900

    chore: 타입스크립트 걷어내기
    일단 타입스크립트 걷어내고 시작
    설정이 너무 복잡함

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from '../components/App';
+import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/잡팅/i);
+  const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
